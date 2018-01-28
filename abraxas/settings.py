63747fcd9cf2b326c37ef0cbd6d1f98d01701d2d
angel lagunas <angel.david.lagunas@gmail.com>
2018-01-28 17:59:18 -0600

    (FEAT:SETTINGS) add postgres as default db

--- abraxas/settings.py
+++ abraxas/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'abraxas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
@@ -118,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+ALLOWED_HOSTS = ['*']
